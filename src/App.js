7db97292fd301c8a4aa3e1dbfbd5de2bb7557b21
unknown <tsedenawit3@gmail.com>
2024-06-27 07:50:50 -0700

    sending

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Definite from './Definite';
-import { MyComponent } from './Congra';
 function App() {
   return (
     <div>
     <Definite />
-    <MyComponent/>
     </div>
   );
 }
