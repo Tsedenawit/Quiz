d9b471e62bd4060d4f824937f4c2251e8935b592
unknown <tsedenawit3@gmail.com>
2024-06-29 09:44:23 +0300

    u

--- src/Definite.jsx
+++ src/Definite.jsx
@@ -4,13 +4,10 @@ export default function Definite(){
     const numbers = [{id:1, num:1},
       {id:2, num:2} ,{id:3, num:3}, {id:4, num:4},{id:5, num:5},
       {id:6, num:6},{id:7, num:7},{id:8, num:8},{id:9, num:9},{id:10, num:10}];
-   
-   
-   
-    const questions =["In which country are the world’s 10 coldest cities located? - russia",
-      "Supposedly home to a “monster,” Loch Ness is one of many inland seas—or “lochs”—in which country?- scotland",
-    "Which country has three capital cities—Pretoria, Cape Town, and Bloemfontein? - southafrica",
-  "Thanks to its overseas territories, which country technically spans 12 time zones? -france",
+    const questions =[{ ID:1 ,que:"In which country are the world’s 10 coldest cities located? - russia"},
+      {ID:2, que:"Supposedly home to a “monster,” Loch Ness is one of many inland seas—or “lochs”—in which country?- scotland"},
+    {ID:3, que:"Which country has three capital cities—Pretoria, Cape Town, and Bloemfontein? - southafrica"},
+  {ID:4, que:"Thanks to its overseas territories, which country technically spans 12 time zones? -france"},
 "With an area of less than 0.2 square mile (0.5 square kilometer), which is the smallest country in the world? - vatican city",
 "Which continent is home to the most countries? -africa",
 "Home to languages like Tok Pisin, Hiri Motu, and a German creole known as Unserdeutsch, which is the most linguistically diverse country in the world? - Papua New Guinea",
@@ -44,9 +41,11 @@ useEffect(()=>{ fetch('https://restcountries.com/v3.1/all')
           </div>
         ))}
         </div>
-        <div>
-          
-        </div>
+        {/* <div>
+          {questions.map((dat)=>{
+            <h1 key={dat.ID}>{dat.que}</h1>
+          })}
+        </div> */}
       </div>
     )
 }
